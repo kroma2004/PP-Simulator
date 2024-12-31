01f1a42f2f4f8648c2b4cc982deec7d3df867890
Maciej Kromka <32202391+kroma2004@users.noreply.github.com>
2024-12-31 17:55:21 +0100

    Prostokąty

---
+++ Simulator/Point.cs
@@ -0,0 +1,31 @@
+﻿using Simulator;
+
+public readonly struct Point
+{
+    public readonly int X, Y;
+    public Point(int x, int y) => (X, Y) = (x, y);
+    public override string ToString() => $"({X}, {Y})";
+
+    public Point Next(Direction direction)
+    {
+        return direction switch
+        {
+            Direction.Right => new Point(X + 1, Y),
+            Direction.Left => new Point(X - 1, Y),
+            Direction.Up => new Point(X, Y + 1),
+            Direction.Down => new Point(X, Y - 1),
+            _ => this
+        };
+    }
+    public Point NextDiagonal(Direction direction)
+    {
+        return direction switch
+        {
+            Direction.Right => new Point(X + 1, Y - 1),
+            Direction.Up => new Point(X + 1, Y + 1),
+            Direction.Left => new Point(X - 1, Y + 1),
+            Direction.Down => new Point(X - 1, Y - 1),
+            _ => this
+        };
+    }
+}
